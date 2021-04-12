e9be6db24bf14738dcb244ccfef351c3cc586e9f
Mike Kolesnik <mkolesni@redhat.com>
2021-04-12 12:38:47 +0300

    Extract common AWS error checking
    This will be useful for validations as well
    
    Signed-off-by: Mike Kolesnik <mkolesni@redhat.com>

--- pkg/aws/errors.go
+++ pkg/aws/errors.go
@@ -1,6 +1,10 @@
 package aws
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+)
 
 type notFoundError struct {
 	s string
@@ -17,4 +21,12 @@ func newNotFoundError(msg string, args ...interface{}) error {
 func isNotFoundError(err error) bool {
 	_, ok := err.(*notFoundError)
 	return ok
+}
+
+func isAWSError(err error, code string) bool {
+	if awsErr, ok := err.(awserr.Error); ok {
+		// Has to be checked as string, see https://github.com/aws/aws-sdk-go/issues/3235
+		return awsErr.Code() == code
+	}
+	return false
 }
