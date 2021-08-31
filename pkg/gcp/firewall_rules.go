fcef6e626d4b309155d895cd95354b0b3c3f6859
Sridhar Gaddam <sgaddam@redhat.com>
2021-08-31 13:53:49 +0530

    GCP support for deploying dedicated Gateway nodes
    This PR implements the following
    1. Supports deploying new Gateway nodes using the OCP machineset API
    2. Cleanup of an existing gateway nodes and firewall rules
    3. Fixes the public-firewall-ports to be applied only to gateway nodes
    
    Fixes issue: https://github.com/submariner-io/cloud-prepare/issues/91
    Signed-Off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/gcp/firewall_rules.go
+++ pkg/gcp/firewall_rules.go
@@ -26,15 +26,24 @@ import (
 )
 
 const (
-	ingressDirection      = "INGRESS"
-	publicPortsRuleName   = "submariner-public-ports"
-	internalPortsRuleName = "submariner-internal-ports"
+	ingressDirection         = "INGRESS"
+	publicPortsRuleName      = "submariner-public-ports"
+	internalPortsRuleName    = "submariner-internal-ports"
+	submarinerGatewayNodeTag = "submariner-io-gateway-node"
 )
 
 func newExternalFirewallRules(projectID, infraID string, ports []api.PortSpec) (ingress *compute.Firewall) {
 	ingressName := generateRuleName(infraID, publicPortsRuleName)
 
-	return newFirewallRule(projectID, infraID, ingressName, ingressDirection, ports)
+	// We want the external firewall rules to be applied only to Gateway nodes. So, we use the TargetTags
+	// field and include submarinerGatewayNodeTag for selection of Gateway nodes. All the Submariner Gateway
+	// instances will be tagged with submarinerGatewayNodeTag.
+	ingressRule := newFirewallRule(projectID, infraID, ingressName, ingressDirection, ports)
+	ingressRule.TargetTags = []string{
+		submarinerGatewayNodeTag,
+	}
+
+	return ingressRule
 }
 
 func newInternalFirewallRule(projectID, infraID string, ports []api.PortSpec) *compute.Firewall {
@@ -55,11 +64,16 @@ func newInternalFirewallRule(projectID, infraID string, ports []api.PortSpec) *c
 
 func newFirewallRule(projectID, infraID, name, direction string, ports []api.PortSpec) *compute.Firewall {
 	allowedPorts := []*compute.FirewallAllowed{}
+
 	for _, port := range ports {
-		allowedPorts = append(allowedPorts, &compute.FirewallAllowed{
+		fwRule := &compute.FirewallAllowed{
 			IPProtocol: port.Protocol,
-			Ports:      []string{strconv.Itoa(int(port.Port))},
-		})
+		}
+		if port.Port != 0 {
+			fwRule.Ports = []string{strconv.Itoa(int(port.Port))}
+		}
+
+		allowedPorts = append(allowedPorts, fwRule)
 	}
 
 	return &compute.Firewall{
