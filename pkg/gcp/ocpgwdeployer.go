7a7f8aa2ea5ee12ce0ae7c74280064153998d4f6
Tom Pantelis <tompantelis@gmail.com>
2021-10-01 19:58:10 -0400

    Return error from GCP GWDeployer for insufficient nodes
    It reports it as a failure but should also return an error so
    the add-on can set the proper status condition.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/gcp/ocpgwdeployer.go
+++ pkg/gcp/ocpgwdeployer.go
@@ -71,7 +71,9 @@ func (d *ocpGatewayDeployer) Deploy(input api.GatewayDeployInput, reporter api.R
 		return reportFailure(reporter, err, "error parsing current gateway instances")
 	}
 
-	if numGatewayNodes == input.Gateways {
+	gatewayNodesToDeploy := input.Gateways - numGatewayNodes
+
+	if gatewayNodesToDeploy == 0 {
 		reporter.Succeeded("Current gateways match the required number of gateways")
 		return nil
 	}
@@ -79,63 +81,63 @@ func (d *ocpGatewayDeployer) Deploy(input api.GatewayDeployInput, reporter api.R
 	// Currently, we only support increasing the number of Gateway nodes which could be a valid use-case
 	// to convert a non-HA deployment to an HA deployment. We are not supporting decreasing the Gateway
 	// nodes (for now) as it might impact the datapath if we accidentally delete the active GW node.
-	if numGatewayNodes < input.Gateways {
-		gatewayNodesToDeploy := input.Gateways - numGatewayNodes
+	if gatewayNodesToDeploy < 0 {
+		reporter.Failed(fmt.Errorf("decreasing the number of Gateway nodes is not currently supported"))
+		return nil
+	}
 
-		if d.dedicatedGWNode {
-			for _, zone := range eligibleZonesForGW.Elements() {
-				reporter.Started(fmt.Sprintf("Deploying dedicated gateway node in zone %q", zone))
-				err = d.deployGateway(zone)
-				if err != nil {
-					return reportFailure(reporter, err, "error deploying gateway for zone %q", zone)
-				}
-
-				gatewayNodesToDeploy--
-				if gatewayNodesToDeploy <= 0 {
-					reporter.Succeeded("Successfully deployed gateway node")
-					return nil
-				}
+	if d.dedicatedGWNode {
+		for _, zone := range eligibleZonesForGW.Elements() {
+			reporter.Started(fmt.Sprintf("Deploying dedicated gateway node in zone %q", zone))
+			err = d.deployGateway(zone)
+			if err != nil {
+				return reportFailure(reporter, err, "error deploying gateway for zone %q", zone)
 			}
-		} else {
-			// Query the list of instances in the eligibleZones of the current region and if it's a worker node,
-			// configure the instance as Submariner Gateway node.
-			for _, zone := range eligibleZonesForGW.Elements() {
-				workerNodes, err := d.k8sClient.ListNodesWithLabel("topology.kubernetes.io/zone=" + zone + ",node-role.kubernetes.io/worker")
-				if err != nil {
-					return reportFailure(reporter, err, "failed to list k8s nodes in zone %q of project %q", zone, d.ProjectID)
-				}
 
-				for _, node := range workerNodes.Items {
-					machineSetInfo := node.GetAnnotations()["machine.openshift.io/machine"]
-					if machineSetInfo != "" {
-						gcpInstanceInfo := strings.Split(machineSetInfo, "/")
-						if len(gcpInstanceInfo) > 1 {
-							reporter.Started(fmt.Sprintf("Configuring worker node %q in zone %q as gateway node", node.Name, zone))
-							if err := d.configureExistingNodeAsGW(zone, gcpInstanceInfo[1], node.Name); err != nil {
-								return reportFailure(reporter, err, "error configuring gateway node %q", node.Name)
-							}
-							gatewayNodesToDeploy--
-							break
-						}
-					}
-				}
-
-				if gatewayNodesToDeploy <= 0 {
-					reporter.Succeeded("Successfully deployed gateway node")
-					return nil
-				}
+			gatewayNodesToDeploy--
+			if gatewayNodesToDeploy <= 0 {
+				reporter.Succeeded("Successfully deployed gateway node")
+				return nil
 			}
 		}
+	} else {
+		// Query the list of instances in the eligibleZones of the current region and if it's a worker node,
+		// configure the instance as Submariner Gateway node.
+		for _, zone := range eligibleZonesForGW.Elements() {
+			workerNodes, err := d.k8sClient.ListNodesWithLabel("topology.kubernetes.io/zone=" + zone + ",node-role.kubernetes.io/worker")
+			if err != nil {
+				return reportFailure(reporter, err, "failed to list k8s nodes in zone %q of project %q", zone, d.ProjectID)
+			}
 
-		// We try to deploy a single Gateway node per zone (in the selected region). If the numGateways
-		// is more than the number of Zones, its treated as an error.
-		if gatewayNodesToDeploy > 0 {
-			reporter.Failed(fmt.Errorf("there are insufficient zones to deploy the required number of gateways"))
-			return nil
+			for _, node := range workerNodes.Items {
+				machineSetInfo := node.GetAnnotations()["machine.openshift.io/machine"]
+				if machineSetInfo != "" {
+					gcpInstanceInfo := strings.Split(machineSetInfo, "/")
+					if len(gcpInstanceInfo) > 1 {
+						reporter.Started(fmt.Sprintf("Configuring worker node %q in zone %q as gateway node", node.Name, zone))
+						if err := d.configureExistingNodeAsGW(zone, gcpInstanceInfo[1], node.Name); err != nil {
+							return reportFailure(reporter, err, "error configuring gateway node %q", node.Name)
+						}
+						gatewayNodesToDeploy--
+						break
+					}
+				}
+			}
+
+			if gatewayNodesToDeploy <= 0 {
+				reporter.Succeeded("Successfully deployed gateway node")
+				return nil
+			}
 		}
 	}
 
-	return nil
+	// We try to deploy a single Gateway node per zone (in the selected region). If the numGateways
+	// is more than the number of Zones, its treated as an error.
+	err = fmt.Errorf("there are an insufficient number of zones (%d) to deploy the desired number of gateways (%d)",
+		eligibleZonesForGW.Size(), input.Gateways)
+	reporter.Failed(err)
+
+	return err
 }
 
 func (d *ocpGatewayDeployer) parseCurrentGatewayInstances(reporter api.Reporter) (int, stringset.Interface, error) {
