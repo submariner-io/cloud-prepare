c8e7c34fb50ee12ce8ec936d23775e498a633795
Stephen Kitt <skitt@redhat.com>
2022-08-18 11:29:44 +0200

    Limit Azure machine name lengths to 40 characters
    To avoid hitting the 63-character limit on "machine IP names", which
    are generated using the provided machine names, limit the latter to 40
    characters.
    
    The MachineName function is exported for the unit test to limit the
    changes required for this fix; a proper test would require a full
    driver setup as in other packages.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ pkg/azure/ocpgwdeployer_test.go
@@ -0,0 +1,43 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package azure_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/submariner-io/cloud-prepare/pkg/azure"
+)
+
+func TestAzure(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Azure Suite")
+}
+
+var _ = Describe("OCP Gateway Deployer", func() {
+	Describe("MachineName", func() {
+		It("should be at most 40 characters in length", func() {
+			Expect(len(azure.MachineName("acmqe-clc-auto-azure6-fd55b", "centralus", "3"))).To(BeNumerically("<=", 40))
+			Expect(len(azure.MachineName("acmqe-clc-auto-azure6-fd55b", "centraleurope", "3"))).To(BeNumerically("<=", 40))
+			Expect(len(azure.MachineName("acmqe-clc-auto-azure6-fd55b", "centralus", "bigzone"))).To(BeNumerically("<=", 40))
+			Expect(azure.MachineName("acmqe-clc-auto-azure6-fd55b", "us", "1")).To(HavePrefix("acmqe-clc-auto-azure6-fd55b"))
+		})
+	})
+})
